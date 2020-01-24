b0b3959f717fea3522c9bee570b824ab1459e817
Daniel Mayle <dmayle012@gmail.com>
2020-01-24 23:39:14 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -25,6 +25,15 @@ def turtles
     ]
 end
 
+# def turtle_traits(turtles)
+#   turtles.map do |turtle|
+#     turtle[:traits].each do |trait|
+#       trait
+#     end
+#   end
+# end
+
+
 def turtle_traits(turtles)
   turtles.map do |turtle|
     turtle[:traits].each do |trait|
